61789d8298ef2a06c9726d2b84a7877455d4b254
platipenguin <elliotm555@gmail.com>
2018-02-09 12:21:25 -0500

    Stable build

---
+++ CloseBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseBehavior : MonoBehaviour {
+
+    public Material baseMaterial;
+    public Material closeMaterial;
+    public CloseBehavior other;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material = baseMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, other.transform.position) < 5)
+        {
+            GetComponent<Renderer>().material = closeMaterial;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = baseMaterial;
+        }
+	}
+}
